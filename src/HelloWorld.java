d0f85eb0b27bfc8b1c5704e804e3eb84b694230d
Piyanan.K <Duck_comen@hotmail.com>
2015-11-17 21:51:31 +0700

    Branch helloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -12,7 +12,8 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub  
+		System.out.println("lesson_1");
 		System.out.println("Hello World");
 
 	}
