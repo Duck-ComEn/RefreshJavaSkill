df1d819dc92b63b573fe90bfc6ff866d7855ebaa
Piyanan.K <duck_comen@hotmail.com>
2015-11-17 20:45:56 +0700

    initial	hello world

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author AMD_FX8350
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
